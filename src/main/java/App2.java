50d23c31ee4e5fc66d42c58be4bd92e45295ca13
mihe7 <mihe7@gmx.de>
2020-09-14 04:59:09 +0200

    App2 erstellt. Bewegung möglich

---
+++ src/main/java/App2.java
@@ -0,0 +1,69 @@
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
+import javax.swing.*;
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.UncheckedIOException;
+
+public class App2 {
+
+    private MazeWorld world;
+    private WorldView worldView;
+
+    private KeyListener keyboardController = new KeyAdapter() {
+        @Override
+        public void keyPressed(KeyEvent e) {
+            int dx = 0, dy = 0;
+
+            switch (e.getKeyCode()) {
+                case KeyEvent.VK_LEFT:  dx--; break;
+                case KeyEvent.VK_RIGHT: dx++; break;
+                case KeyEvent.VK_UP:    dy--; break;
+                case KeyEvent.VK_DOWN:  dy++; break;
+                default:
+                    return;
+            }
+
+            world.movePlayer(dx, dy);
+            worldView.repaint();
+        }
+    };
+
+    public void run() {
+        world = createWorld();
+        worldView = new WorldView(world, 32, 32);
+        WorldViewDefaults.configure(worldView);
+
+        worldView.setFocusable(true);
+        worldView.addKeyListener(keyboardController);
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.add(worldView);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private MazeWorld createWorld() {
+        String text = 
+            "wwwwwdw\n" +
+            "wfddddw\n" +
+            "wdddddd\n" +
+            "ddddddw\n" +
+            "wdddddw\n" +
+            "wwwwwdw\n";
+             
+        MazeTextFormat fmt = new MazeTextFormat();
+        try {
+            Maze maze = fmt.read(new StringReader(text));
+            return new MazeWorld(maze, new Position(4, 4));
+        } catch (IOException ex) {
+            throw new UncheckedIOException(ex);
+        }
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new App2().run());
+    }
+}
