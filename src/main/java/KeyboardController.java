099a347d8d1eb8ee81fad304ec091c9d127f48b4
mihe7 <mihe7@gmx.de>
2020-09-19 12:17:51 +0200

    Direction im KeyboardController berücksichtigt.

--- src/main/java/KeyboardController.java
+++ src/main/java/KeyboardController.java
@@ -1,7 +1,18 @@
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.util.Map;
+import java.util.HashMap;
 
 public class KeyboardController extends KeyAdapter {
+    private static final Map<Integer, Direction> KEY_MAPPING =
+            new HashMap<>();
+    static {
+        KEY_MAPPING.put(KeyEvent.VK_LEFT, Direction.WEST);
+        KEY_MAPPING.put(KeyEvent.VK_RIGHT, Direction.EAST);
+        KEY_MAPPING.put(KeyEvent.VK_UP, Direction.NORTH);
+        KEY_MAPPING.put(KeyEvent.VK_DOWN, Direction.SOUTH);
+    }
+
     private final MazeGame game;
 
     public KeyboardController(MazeGame game) {
@@ -10,18 +21,10 @@ public class KeyboardController extends KeyAdapter {
 
     @Override
     public void keyPressed(KeyEvent e) {
-        int dx = 0, dy = 0;
-
-        switch (e.getKeyCode()) {
-            case KeyEvent.VK_LEFT:  dx--; break;
-            case KeyEvent.VK_RIGHT: dx++; break;
-            case KeyEvent.VK_UP:    dy--; break;
-            case KeyEvent.VK_DOWN:  dy++; break;
-            default:
-                return;
+        Direction dir = KEY_MAPPING.get(e.getKeyCode());
+        if (dir != null) {
+            game.movePlayer(dir);
         }
-
-        game.movePlayer(dx, dy);
     }
 }
 
