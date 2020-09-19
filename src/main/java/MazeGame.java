17bf314f5bb22a519bae0cbd1faf82fa7fd4b452
mihe7 <mihe7@gmx.de>
2020-09-19 11:45:51 +0200

    GameStateObservers bei MazeGame berücksichtigt

--- src/main/java/MazeGame.java
+++ src/main/java/MazeGame.java
@@ -1,8 +1,13 @@
 public class MazeGame {
     private MazeWorld world;
+    private GameStateObserver observer;
 
     public MazeGame(MazeWorld world) {
         this.world = world;
+    }
+
+    public void setObserver(GameStateObserver observer) {
+        this.observer = observer;
     }
 
     public MazeWorld getWorld() { return world; }
@@ -15,7 +20,12 @@ public class MazeGame {
         if (isWin()) {
             return;
         }
+
         world.movePlayer(dx, dy);
+
+        if (isWin()) {
+            observer.gameOver();
+        }
     }
 }
 
